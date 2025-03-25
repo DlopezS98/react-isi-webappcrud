9ee3198303c3c0392071a144bb4098b8cc44087c
DlopezS98 <01dlopezs98@gmail.com>
2025-03-25 14:50:24 -0600

    feat: enhance create product page with additional fields and improved layout

--- src/admin/products/create.tsx
+++ src/admin/products/create.tsx
@@ -1,16 +1,51 @@
+import Button from '../../components/button';
 import Card, { CardBody, CardHeader } from '../../components/card';
+import SaveIcon from '@mui/icons-material/Save';
 
 export default function CreateProductPage() {
   return (
     <Card>
       <CardHeader>
-        <h2>Create new product</h2>
+        <div className='mx-3'>
+          <h2>Create new product</h2>
+        </div>
       </CardHeader>
       <CardBody>
-        <form className='form'>
-          <div className='form__group'>
-            <label htmlFor='name'>Product name</label>
-            <input type='text' id='name' placeholder='Product name' />
+        <form className='form shadow-md'>
+          <div className='row gap-2 justify-center align-center'>
+            <div className='column form__group'>
+              <label htmlFor='name'>Product name</label>
+              <input type='text' id='name' name='name' placeholder='Product name' />
+            </div>
+            <div className='column form__group'>
+              <label htmlFor='category'>Category</label>
+              <select id='category' name='category' className='select'>
+                <option value=''>Select category</option>
+                <option value='1'>Category 1</option>
+                <option value='2'>Category 2</option>
+              </select>
+            </div>
+          </div>
+          <div className='row gap-2 justify-center align-center'>
+            <div className='column form__group'>
+              <label htmlFor='price'>Price</label>
+              <input type='number' id='price' name='price' placeholder='Product price' />
+            </div>
+            <div className='column form__group'>
+              <label htmlFor='quantity'>Quantity</label>
+              <input type='number' id='quantity' name='quantity' placeholder='Product quantity' />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='column form__group'>
+              <label htmlFor='description'>Description</label>
+              <textarea id='description' name='description' placeholder='Product description' rows={5}></textarea>
+            </div>
+          </div>
+          <div className='column'>
+            <Button onClick={() => {}} variant='primary'>
+              <SaveIcon /> Create Product
+            </Button>
           </div>
         </form>
       </CardBody>
