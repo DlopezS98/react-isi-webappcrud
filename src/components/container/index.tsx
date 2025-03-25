76a68f15b891f25100d8cde40e190e889347c8e8
DlopezS98 <01dlopezs98@gmail.com>
2025-03-25 11:05:02 -0600

    Setup routes

---
+++ src/components/container/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ContainerProps extends React.PropsWithChildren<{}> {
+	className?: string ;
+}
+
+export default function Container(props: ContainerProps) {
+	return <div className={`container ${props.className}`}>{props.children}</div>;
+}
