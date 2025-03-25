e24536b7f57b460d7bbd5602d67170e67e4f4504
DlopezS98 <01dlopezs98@gmail.com>
2025-03-25 12:16:19 -0600

    feat: implement Card component and update ProductsPage layout

--- src/admin/products/products.page.tsx
+++ src/admin/products/products.page.tsx
@@ -1,9 +1,16 @@
+import Card, { CardBody, CardHeader } from "../../components/card";
+
 export interface ProductsPageProps {}
 
 const ProductsPage: React.FC<ProductsPageProps> = () => {
   return (
     <div className="container py-3">
-      <h2>Products Page</h2>
+      <Card>
+        <CardHeader><h2>Products Page</h2></CardHeader>
+        <CardBody>
+          <p>Products page content</p>
+        </CardBody>
+      </Card>
     </div>
   );
 };
---
+++ src/components/card/index.tsx
@@ -0,0 +1,21 @@
+import './card.css';
+
+export interface CardProps {}
+
+const Card: React.FC<React.PropsWithChildren<CardProps>> = (props) => {
+  return <div className='isi-card shadow-md'>{props.children}</div>;
+};
+export default Card;
+
+export interface CardHeaderProps {}
+
+export const CardHeader: React.FC<React.PropsWithChildren<CardHeaderProps>> = (props) => {
+  return <div className='isi-card__header'>{props.children}</div>;
+};
+
+export interface CardBodyProps {}
+
+export const CardBody: React.FC<React.PropsWithChildren<CardBodyProps>> = (props) => {
+  return <div className='isi-card__body'>{props.children}</div>;
+};
+
